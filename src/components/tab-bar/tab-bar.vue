2bd279320b4a9aaaf1c425ba5ccd45eebf017bb6
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-14 19:13:32 +0300

    add padding to v-bottom-navigation for safe area insets

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -24,3 +24,9 @@ const generateIconColor = (path: string): string =>
     />
   </v-bottom-navigation>
 </template>
+
+<style>
+.v-bottom-navigation {
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
