47bdb6e32ae38e3fcbe2016fd04b94d7c7325216
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-16 17:52:50 +0300

    feat: implement widget component and display today's brews count on dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { WidgetData } from '@/views/dashboard/types'
+import { onMounted, ref, type Ref } from 'vue'
+import Widget from '@/views/dashboard/widget.vue'
+import useBrewsStore from '@/store/brews/brews.ts'
+
+const brewsStore = useBrewsStore()
+
+const widgets: Ref<WidgetData[]> = ref([])
+
+const getBrewsTodayCount = async () => {
+  const brewsTodayCount = await brewsStore.getBrewsTodayCount()
+  widgets.value.push({
+    title: brewsTodayCount,
+    subtitle: `Brew${brewsTodayCount === 1 ? '' : 's'} today`,
+    size: 'half',
+    color: 'deep-orange-lighten-1',
+  })
+}
+
+onMounted(async () => {
+  await getBrewsTodayCount()
+})
+</script>
 
 <template>
-  <h1>dashboard</h1>
+  <div class="d-flex flex-wrap ga-4">
+    <widget v-for="(widget, index) in widgets" :key="index" :data="widget" />
+  </div>
 </template>
 
 <style scoped></style>
