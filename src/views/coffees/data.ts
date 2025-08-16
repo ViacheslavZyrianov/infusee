ad161c421c2de8be6d06240d552e413ae5b59e3a
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-16 16:59:16 +0300

    feat: add new brew methods and refine roast level options for improved selection

--- src/views/coffees/data.ts
+++ src/views/coffees/data.ts
@@ -13,21 +13,31 @@ export const processingOptions: SelectItem[] = [
     title: 'Honey',
     value: 'honey',
   },
+  {
+    title: 'Wet-Hulled',
+    value: 'wet_hulled',
+  },
+  {
+    title: 'Anaerobic Fermentation',
+    value: 'anaerobic_fermentation',
+  },
+  {
+    title: 'Carbonic Maceration',
+    value: 'carbonic_maceration',
+  },
+  {
+    title: 'Experimental / Other',
+    value: 'other',
+  },
 ]
 
 export const roastLevelOptions: SelectItem[] = [
-  {
-    title: 'Light',
-    value: 'light',
-  },
-  {
-    title: 'Medium',
-    value: 'medium',
-  },
-  {
-    title: 'Dark',
-    value: 'dark',
-  },
+  { title: 'Light', value: 'light' },
+  { title: 'Light-Medium', value: 'light_medium' },
+  { title: 'Medium', value: 'medium' },
+  { title: 'Medium-Dark', value: 'medium_dark' },
+  { title: 'Dark', value: 'dark' },
+  { title: 'Very Dark', value: 'very_dark' },
 ]
 
 const findOptionByValue = (options: SelectItem[], value: string): string =>
