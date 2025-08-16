dfc96ece675f3e2a79af40b069e9744be13f7bfa
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-16 16:37:00 +0300

    refactor: improve route handling and icon color generation in tab bar

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import { onMounted, ref, type Ref } from 'vue'
+import { onMounted, ref, type Ref, computed, type ComputedRef } from 'vue'
 import tabBarItems from './items'
 import { useRoute } from 'vue-router'
 import supabase from '@/plugins/supabase.ts'
 
 const avatar: Ref<string> = ref('')
-
 const route = useRoute()
 
-const currentRoute: Ref<string> = ref(String(route.name))
+const currentRoutePath: ComputedRef<string> = computed(() => route.path)
 
-const generateIconColor = (path: string): string =>
-  currentRoute.value === path ? 'primary' : 'grey'
+const generateIconColor = (path: string): 'primary' | 'grey' =>
+  currentRoutePath.value.startsWith(path) ? 'primary' : 'grey'
 
 const getUser = async () => {
   const { data } = await supabase.auth.getSession()
-
   avatar.value = data.session?.user.user_metadata.avatar_url
 }
 
@@ -25,17 +23,16 @@ onMounted(async () => {
 </script>
 
 <template>
-  <v-bottom-navigation v-model="currentRoute" app>
+  <v-bottom-navigation app>
     <v-btn
       v-for="item in tabBarItems"
       :key="item.icon"
       :to="item.path"
-      :value="item.path"
       :icon="item.icon"
       :color="generateIconColor(item.path)"
       block
     />
-    <v-btn :to="'/profile'" value="profile" :color="generateIconColor('profile')" block>
+    <v-btn :to="'/profile'" :color="generateIconColor('/profile')" block>
       <v-avatar size="24">
         <v-img :src="avatar" alt="Profile" />
       </v-avatar>
