69b3fef509006f12495e4db6ff3258f89935f5c8
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-16 19:53:16 +0300

    feat: refactor brew item UI to improve chip display layout

--- src/views/brews/brew-item.vue
+++ src/views/brews/brew-item.vue
@@ -72,17 +72,10 @@ onMounted(async () => {
       </v-menu>
     </div>
 
-    <v-card-subtitle>
-      <v-chip-group>
-        <v-chip
-          v-if="brew.brew_method"
-          :text="brew.brew_method"
-          size="small"
-          prepend-icon="mdi-flask-outline"
-        />
-        <v-chip v-if="brew.roaster" :text="brew.roaster" size="small" prepend-icon="mdi-fire" />
-      </v-chip-group>
-    </v-card-subtitle>
+    <div class="d-flex flex-wrap ga-2">
+      <v-chip v-if="brew.brew_method" :text="brew.brew_method" prepend-icon="mdi-flask-outline" />
+      <v-chip v-if="brew.roaster" :text="brew.roaster" prepend-icon="mdi-fire" />
+    </div>
 
     <v-divider class="my-2" />
 
