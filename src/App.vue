3579b380e3aae9e7b5852bbf6562471ed46e2d32
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-17 23:26:56 +0300

    feat: add global alert component to App.vue for improved user notifications

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@ import { useRoute } from 'vue-router'
 import FullPageLayout from '@/layouts/full-page.vue'
 import DefaultLayout from '@/layouts/default/default.vue'
 import { useSettingsStore } from '@/store/settings'
+import { useAlertStore } from '@/store/alert/alert.ts'
 
 const route = useRoute()
 const settingsStore = useSettingsStore()
+const alert = useAlertStore()
 
 const layout: ComputedRef<unknown> = computed(() => {
   switch (route.meta.layout) {
@@ -23,5 +25,18 @@ const layout: ComputedRef<unknown> = computed(() => {
     <component :is="layout">
       <router-view />
     </component>
+
+    <v-alert
+      v-if="alert.visible"
+      :type="alert.type"
+      border="start"
+      colored-border
+      elevation="2"
+      class="global-alert"
+      dismissible
+      @click:close="alert.hide"
+    >
+      {{ alert.message }}
+    </v-alert>
   </v-theme-provider>
 </template>
