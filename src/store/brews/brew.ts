916d910109ffe4ddce6fbc5d0e65cf97e38f52aa
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-17 20:07:13 +0300

    feat: enhance loading state management with BrewLoading type

--- src/store/brews/brew.ts
+++ src/store/brews/brew.ts
@@ -1,13 +1,38 @@
 import { defineStore } from 'pinia'
-import type { Brew, BrewRead } from '@/store/brews/types.ts'
+import type { Brew, BrewLoading, BrewRead } from '@/store/brews/types.ts'
 import supabase from '@/plugins/supabase.ts'
 import string2number from '@/utilities/string2number.ts'
-import { ref, type Ref } from 'vue'
+import { reactive } from 'vue'
 
 export default defineStore('brew', () => {
-  const isLoading: Ref<boolean> = ref(false)
+  const isLoading: BrewLoading = reactive({
+    getBrew: true,
+    postBrew: true,
+    updateBrew: true,
+    deleteBrew: true,
+  })
+
+  const getBrew = async (brewId: string): Promise<BrewRead> => {
+    isLoading.getBrew = true
+
+    const { data, error } = await supabase
+      .from('brews')
+      .select('*, coffees (name)')
+      .eq('id', brewId)
+      .single()
+
+    isLoading.getBrew = false
+
+    if (error) {
+      throw new Error(`Error fetching brew: ${error.message}`)
+    }
+
+    return data
+  }
 
   const postBrew = async (form: Brew) => {
+    isLoading.postBrew = true
+
     const {
       data: { user },
     } = await supabase.auth.getUser()
@@ -30,30 +55,16 @@ export default defineStore('brew', () => {
 
     const { error } = await supabase.from('brews').insert([payload])
 
+    isLoading.postBrew = false
+
     if (error) {
       throw new Error(`Error posting brew: ${error.message}`)
     }
   }
 
-  const getBrew = async (brewId: string): Promise<BrewRead> => {
-    isLoading.value = true
-
-    const { data, error } = await supabase
-      .from('brews')
-      .select('*, coffees (name)')
-      .eq('id', brewId)
-      .single()
-
-    isLoading.value = false
-
-    if (error) {
-      throw new Error(`Error fetching brew: ${error.message}`)
-    }
-
-    return data
-  }
-
   const updateBrew = async (id: string, form: Brew) => {
+    isLoading.updateBrew = true
+
     const { data } = await supabase.auth.getSession()
     if (!data?.session?.user.id) throw new Error('Not logged in')
 
@@ -74,13 +85,19 @@ export default defineStore('brew', () => {
 
     const { error } = await supabase.from('brews').update(payload).eq('id', id)
 
+    isLoading.updateBrew = false
+
     if (error) {
       throw new Error(`Error updating brew: ${error.message}`)
     }
   }
 
   const deleteBrew = async (id: string) => {
+    isLoading.deleteBrew = true
+
     const { error } = await supabase.from('brews').delete().eq('id', id)
+
+    isLoading.deleteBrew = false
 
     if (error) {
       throw new Error(`Error deleting brew: ${error.message}`)
