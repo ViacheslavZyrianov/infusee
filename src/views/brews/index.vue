aa0c69c58da56621351dbc0692da581f814f1e48
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-17 19:57:43 +0300

    feat: implement loading state for brews retrieval and refactor data handling

--- src/views/brews/index.vue
+++ src/views/brews/index.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import useBrewsStore from '@/store/brews/brews.ts'
 import useBrewStore from '@/store/brews/brew.ts'
-import { onMounted } from 'vue'
+import { onMounted, ref, type Ref } from 'vue'
+import type { BrewRead } from '@/store/brews/types.ts'
 import BrewItem from './brew-item.vue'
 
 const brewsStore = useBrewsStore()
 const brewStore = useBrewStore()
 
+const brews: Ref<BrewRead[]> = ref([])
+
 const onDelete = async (id: string) => {
   await brewStore.deleteBrew(id)
-  await brewsStore.getBrews()
+  await getBrews()
+}
+
+const getBrews = async () => {
+  brews.value = await brewsStore.getBrews()
 }
 
 onMounted(async () => {
-  await brewsStore.getBrews()
+  await getBrews()
 })
 </script>
 
@@ -22,12 +29,10 @@ onMounted(async () => {
     <v-btn prepend-icon="mdi-plus" to="/brews/add">Add brew</v-btn>
   </teleport>
   <div class="d-flex flex-column ga-4">
-    <brew-item
-      v-for="brew in brewsStore.brews"
-      :key="brew.id"
-      :brew="brew"
-      @delete="onDelete(brew.id)"
-    />
+    <v-skeleton-loader v-if="brewsStore.isLoading.getBrews" type="article" height="136px" />
+    <template v-else>
+      <brew-item v-for="brew in brews" :key="brew.id" :brew="brew" @delete="onDelete(brew.id)" />
+    </template>
   </div>
 </template>
 
