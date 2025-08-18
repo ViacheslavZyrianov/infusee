e9991e542ad0838ba0c6ada24e119e5f872d0484
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-18 23:05:21 +0300

    fix: update coffee form reference and add validation before saving

--- src/views/coffees/add/index.vue
+++ src/views/coffees/add/index.vue
@@ -2,28 +2,19 @@
 import { type Ref, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import CoffeeForm from '@/views/coffees/coffee-form.vue'
-import type { Coffee } from '@/store/coffees/types.ts'
 import useCoffeeStore from '@/store/coffees/coffee.ts'
 
 const router = useRouter()
 
 const coffeeStore = useCoffeeStore()
 
-const coffeeFormRef: Ref<{ form: Coffee }> = ref({
-  form: {
-    name: '',
-    cupping_score: null,
-    is_specialty: false,
-    is_public: false,
-    country: null,
-    processing: null,
-    roast_level: '',
-    notes: '',
-    brew_date: null,
-  },
-})
+const coffeeFormRef: Ref<InstanceType<typeof CoffeeForm> | null> = ref(null)
 
 const onSave = async () => {
+  const result = await coffeeFormRef.value?.validate()
+
+  if (!result?.valid || !coffeeFormRef.value) return
+
   await coffeeStore.postCoffee(coffeeFormRef.value.form)
   router.push('/coffees')
 }
@@ -34,7 +25,14 @@ const onSave = async () => {
     <v-btn variant="outlined" to="/coffees">Cancel</v-btn>
   </teleport>
   <teleport defer to="#app-bar-action--right">
-    <v-btn color="success" @click="onSave">Save</v-btn>
+    <v-btn
+      :loading="coffeeStore.isLoading.postCoffee"
+      :disabled="coffeeStore.isLoading.postCoffee"
+      color="success"
+      @click="onSave"
+    >
+      Save
+    </v-btn>
   </teleport>
   <coffee-form ref="coffeeFormRef" />
 </template>
