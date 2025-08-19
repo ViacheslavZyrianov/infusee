7643b40850ef976e8e82f82bd585d0ab4938bdf4
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-19 15:22:32 +0300

    fix: enhance settings store with localStorage persistence

--- src/store/settings/index.ts
+++ src/store/settings/index.ts
@@ -2,24 +2,30 @@ import { defineStore } from 'pinia'
 import { ref, computed, type Ref } from 'vue'
 import useCurrencies from '@/composables/useCurrencies.ts'
 
-export const useSettingsStore = defineStore('settings', () => {
-  const isThemeDark: Ref<boolean> = ref(window.matchMedia('(prefers-color-scheme: dark)').matches)
-  const isShowDayNightCodes: Ref<boolean> = ref(true)
+export const useSettingsStore = defineStore(
+  'settings',
+  () => {
+    const isThemeDark: Ref<boolean> = ref(window.matchMedia('(prefers-color-scheme: dark)').matches)
 
-  const currencies = useCurrencies()
-  const currency: Ref<keyof typeof currencies.currencies> = ref(currencies.defaultCurrency)
+    const currencies = useCurrencies()
+    const currency: Ref<keyof typeof currencies.currencies> = ref(currencies.defaultCurrency)
 
-  const getTheme = computed(() => (isThemeDark.value ? 'dark' : 'light'))
+    const getTheme = computed(() => (isThemeDark.value ? 'dark' : 'light'))
 
-  const toggleTheme = () => {
-    isThemeDark.value = !isThemeDark.value
-  }
+    const toggleTheme = () => {
+      isThemeDark.value = !isThemeDark.value
+    }
 
-  return {
-    isThemeDark,
-    isShowDayNightCodes,
-    currency,
-    getTheme,
-    toggleTheme,
-  }
-})
+    return {
+      isThemeDark,
+      currency,
+      getTheme,
+      toggleTheme,
+    }
+  },
+  {
+    persist: {
+      storage: localStorage,
+    },
+  },
+)
