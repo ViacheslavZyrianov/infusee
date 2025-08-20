3d040554fd4613ed57ad293731ed0f12b8fda382
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-20 17:04:56 +0300

    feat: add installation prompt for Kavoznay web app

---
+++ src/views/not-pwa/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card class="pa-6 text-center">
+    <v-card-title class="text-h5 font-weight-bold mb-4"> Install Kavoznay </v-card-title>
+
+    <v-card-text class="d-flex flex-column ga-4">
+      <div>
+        You’re currently using the web version. It's required to install our app on your device.
+      </div>
+      <div class="text-subtitle-2 text-medium-emphasis">
+        Open the browser menu and<br />select <strong>“Add to Home Screen”</strong>.
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
