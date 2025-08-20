3c28b930595b37ab81bb21da8058db6b667d9d48
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-20 23:43:27 +0300

    feat: update navigation guard to allow access to public URLs

--- src/router/middleware/index.ts
+++ src/router/middleware/index.ts
@@ -1,9 +1,11 @@
 import type { RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
 import detectIsPWA from './isPWA.ts'
 
+const publicURLs = ['/privacy-policy', '/terms-of-service', '/not-pwa']
+
 export default {
   beforeEach(to: RouteLocationNormalized, _: RouteLocationNormalized, next: NavigationGuardNext) {
-    if (!detectIsPWA() && to.path !== '/not-pwa') {
+    if (!detectIsPWA() && !publicURLs.includes(to.path)) {
       next('/not-pwa')
     } else {
       next()
