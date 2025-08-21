cc1d1ab62c8ff0260b6d669cbc8e81c5627bb3b5
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-21 18:50:08 +0300

    feat: integrate settings store to manage i18n locale in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import '@/assets/styles.css'
 import { registerGlobalComponents } from '@/plugins/global-components'
 import piniaPersist from 'pinia-plugin-persistedstate'
 import i18n from './plugins/i18n/index.ts'
+import { useSettingsStore } from '@/store/settings'
 
 import App from './App.vue'
 import router from './router'
@@ -19,5 +20,8 @@ app.use(pinia)
 app.use(router)
 app.use(vuetify)
 app.use(i18n)
+
+const store = useSettingsStore()
+i18n.global.locale.value = store.locale as 'en' | 'uk' // Type assertion to ensure correct type
 
 app.mount('#app')
