674ecc4b3073a82e8bc14ace4188d56d9092dd91
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-25 00:12:59 +0300

    add translations

--- src/views/not-pwa/ios.vue
+++ src/views/not-pwa/ios.vue
@@ -1,13 +1,18 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { VIcon } from 'vuetify/components'
+
+const { tm } = useI18n()
+</script>
+
 <template>
   <v-card class="text-body-1 text-left" variant="tonal" rounded="xl">
     <div class="d-flex align-center justify-center mb-2">
       <v-icon icon="mdi-apple" size="48" />
     </div>
-    <ol class="pl-6">
-      <li>Open this app in <strong>Safari</strong>.</li>
-      <li>Tap the <strong>Share</strong> icon <v-icon icon="mdi-export-variant" />.</li>
-      <li>Scroll down and tap <strong>Add to Home Screen</strong>.</li>
-      <li>Tap <strong>Add</strong> in the top-right corner.</li>
+
+    <ol>
+      <li v-for="step in tm('not_pwa.ios.steps')" :key="step" v-html="step" />
     </ol>
   </v-card>
 </template>
