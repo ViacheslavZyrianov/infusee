69463193c2e8481999997612e0f4fb413a1dfcfb
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-24 19:15:27 +0300

    remove commented code

--- src/router/routes/roasters.ts
+++ src/router/routes/roasters.ts
@@ -4,14 +4,4 @@ export default [
     name: 'roasters',
     component: () => import('@/views/roasters/index.vue'),
   },
-  // {
-  //   path: '/brews/add',
-  //   name: 'Add brew',
-  //   component: () => import('@/views/brews/add/index.vue'),
-  // },
-  // {
-  //   path: '/brews/edit/:id',
-  //   name: 'Edit brew',
-  //   component: () => import('@/views/brews/edit/[id]/index.vue'),
-  // },
 ]
