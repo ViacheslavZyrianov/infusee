48b6d1d44049e1f97af22e033400d109679e873b
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-24 17:15:18 +0300

    handle page titles from i18n

--- src/layouts/default/default.vue
+++ src/layouts/default/default.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-import { computed, type ComputedRef } from 'vue'
 import { useRoute } from 'vue-router'
+import { useI18n } from 'vue-i18n'
 import TabBar from './tab-bar/tab-bar.vue'
 
 const route = useRoute()
-
-const title: ComputedRef<string> = computed(() => (route.name as string) || '')
+const { t } = useI18n()
 </script>
 
 <template>
   <v-app>
     <v-app-bar app class="d-flex justify-space-between px-2">
-      <div class="position-absolute left-0 right-0 my-auto px-16 text-center text-truncate">
-        {{ title }}
+      <div
+        class="position-absolute left-0 right-0 my-auto px-16 text-center text-truncate text-body-2"
+      >
+        {{ t(`${route.name as string}.title`) }}
       </div>
       <template v-slot:prepend>
         <div id="app-bar-action--left" />
