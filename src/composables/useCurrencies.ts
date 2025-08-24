b082829c86cdc434dc3e66804ab492589cf75c73
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-24 19:13:42 +0300

    refactor and improve useCurrencies.ts

--- src/composables/useCurrencies.ts
+++ src/composables/useCurrencies.ts
@@ -1,28 +1,27 @@
 import type { SelectItem } from '@/types/types.ts'
+import i18n from '@/plugins/i18n'
+import { computed, type ComputedRef } from 'vue'
 
 export default () => {
-  const currencies = {
-    UAH: { title: 'Ukrainian Hryvnia', symbol: '₴' },
-    USD: { title: 'US Dollar', symbol: '$' },
-    EUR: { title: 'Euro', symbol: '€' },
-    CAD: { title: 'Canadian Dollar', symbol: 'C$' },
-  }
+  const currencies = computed(() => i18n.global.messages.value[i18n.global.locale.value].currencies)
 
-  const defaultCurrency: keyof typeof currencies = 'UAH'
+  const defaultCurrency: keyof typeof currencies.value = 'UAH'
 
-  const currencyOptions: SelectItem[] = Object.entries(currencies).map(([value, { title }]) => ({
-    title,
-    value,
-  }))
+  const currencyOptions: ComputedRef<SelectItem[]> = computed(() =>
+    Object.entries(currencies.value).map(([value, { title }]) => ({
+      title,
+      value,
+    })),
+  )
 
-  const getCurrencyTitle = (currency: keyof typeof currencies): string =>
-    currencies[currency]?.title || ''
+  const getCurrencyTitle = (currency: keyof typeof currencies.value): string =>
+    currencies.value[currency]?.title || ''
 
-  const getCurrencySymbol = (currency: keyof typeof currencies): string =>
-    currencies[currency]?.symbol || ''
+  const getCurrencySymbol = (currency: keyof typeof currencies.value): string =>
+    currencies.value[currency]?.symbol || ''
 
-  const getCurrencyTitleAndSymbol = (currency: keyof typeof currencies): string =>
-    `${currencies[currency]?.title} ${currencies[currency]?.symbol}` || ''
+  const getCurrencyTitleAndSymbol = (currency: keyof typeof currencies.value): string =>
+    `${currencies.value[currency]?.title} ${currencies.value[currency]?.symbol}` || ''
 
   return {
     currencies,
