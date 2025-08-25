b6d0b8a031c4ea901e942ac4b9d015b9c37dadd3
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-25 14:02:12 +0300

    add type RatingsObject

--- src/types/brews.ts
+++ src/types/brews.ts
@@ -3,3 +3,13 @@ export type BrewMethod = {
   icon: `mdi-${string}`
   methods: Record<string, string>
 }
+
+/** Type for all ratings, keyed by their friendly name */
+export type RatingsObject = Record<
+  string,
+  {
+    model: string
+    label: string
+    hint: string
+  }
+>
