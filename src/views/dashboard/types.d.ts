21b2ec3dd244672f4bb550a6ce5a48777aa69fa2
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-26 18:50:16 +0300

    add prop component to type WidgetData

--- src/views/dashboard/types.d.ts
+++ src/views/dashboard/types.d.ts
@@ -1,7 +1,8 @@
-import type { Ref } from 'vue'
+import type { Component, Ref } from 'vue'
 
 export type WidgetData = {
   title: string | number | Ref<string>
+  component?: Component
   subtitle?: string
   size: 'half' | 'full'
   color?: string
