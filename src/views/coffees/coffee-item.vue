607b00b06171686e74c49a30721dfe0d5a647e8a
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-27 17:29:40 +0300

    refactor coffee item

--- src/views/coffees/coffee-item.vue
+++ src/views/coffees/coffee-item.vue
@@ -97,22 +97,23 @@ const onDelete = () => {
           </v-chip>
         </div>
       </div>
-
-      <v-divider class="my-2" />
     </template>
 
-    <div v-if="isRoasterDetails" class="d-flex align-center flex-wrap ga-2">
-      <v-icon icon="mdi-fire" size="18" />
-      <v-chip v-if="coffee.roasters?.title" size="small">
-        {{ coffee.roasters.title }}
-      </v-chip>
-      <v-chip v-if="coffee.roast_level" size="small">
-        {{ getRoastLevelOptionTitleByValue(coffee.roast_level) }}
-      </v-chip>
-      <v-chip v-if="formattedDate" size="small">
-        {{ formattedDate }}
-      </v-chip>
-    </div>
+    <template v-if="isRoasterDetails">
+      <v-divider class="my-2" />
+      <div class="d-flex align-center flex-wrap ga-2">
+        <v-icon icon="mdi-fire" size="18" />
+        <v-chip v-if="coffee.roasters?.title" size="small">
+          {{ coffee.roasters.title }}
+        </v-chip>
+        <v-chip v-if="coffee.roast_level" size="small">
+          {{ getRoastLevelOptionTitleByValue(coffee.roast_level) }}
+        </v-chip>
+        <v-chip v-if="formattedDate" size="small">
+          {{ formattedDate }}
+        </v-chip>
+      </div>
+    </template>
 
     <template v-if="coffee.notes">
       <v-divider class="mt-2" />
