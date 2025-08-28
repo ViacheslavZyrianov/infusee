a227ed4652145825f8644d4aee8c9d99475ab4e2
Viacheslav Zyrianov <zyrianov.viacheslav@gmail.com>
2025-08-28 15:47:55 +0300

    change Terms Of Service to Terms of Service

--- src/views/not-pwa/footer-info.vue
+++ src/views/not-pwa/footer-info.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer class="position-fixed bottom-0 right-0 left-0 flex justify-center">
     <v-btn to="/terms-of-service" variant="text" class="text-decoration-underline">
-      Terms Of Service
+      Terms of Service
     </v-btn>
     <v-btn to="/privacy-policy" variant="text" class="text-decoration-underline">
       Privacy Policy
