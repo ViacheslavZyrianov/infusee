7bccbba3aadb201be3aecaaa24cf3b1e207b2c35
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-08-28 18:43:47 +0300

    generate types from supabase

---
+++ src/types/database.types.ts
@@ -0,0 +1,302 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.4"
+  }
+  public: {
+    Tables: {
+      brews: {
+        Row: {
+          brew_method: string | null
+          channeling: number | null
+          coffee_id: string | null
+          created_at: string | null
+          dose: number | null
+          grind: number | null
+          id: string
+          notes: string | null
+          output: number | null
+          rating_acidity: number | null
+          rating_aftertaste: number
+          rating_aroma: number | null
+          rating_bitterness: number | null
+          rating_body: number | null
+          rating_flavor: number | null
+          rating_sweetness: number | null
+          user_id: string
+        }
+        Insert: {
+          brew_method?: string | null
+          channeling?: number | null
+          coffee_id?: string | null
+          created_at?: string | null
+          dose?: number | null
+          grind?: number | null
+          id?: string
+          notes?: string | null
+          output?: number | null
+          rating_acidity?: number | null
+          rating_aftertaste: number
+          rating_aroma?: number | null
+          rating_bitterness?: number | null
+          rating_body?: number | null
+          rating_flavor?: number | null
+          rating_sweetness?: number | null
+          user_id: string
+        }
+        Update: {
+          brew_method?: string | null
+          channeling?: number | null
+          coffee_id?: string | null
+          created_at?: string | null
+          dose?: number | null
+          grind?: number | null
+          id?: string
+          notes?: string | null
+          output?: number | null
+          rating_acidity?: number | null
+          rating_aftertaste?: number
+          rating_aroma?: number | null
+          rating_bitterness?: number | null
+          rating_body?: number | null
+          rating_flavor?: number | null
+          rating_sweetness?: number | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "brews_coffee_id_fkey"
+            columns: ["coffee_id"]
+            isOneToOne: false
+            referencedRelation: "coffees"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      coffees: {
+        Row: {
+          country: string | null
+          cupping_score: number | null
+          id: string
+          is_public: boolean | null
+          is_specialty: boolean | null
+          name: string
+          notes: string | null
+          price: number | null
+          processing: string | null
+          roast_date: string | null
+          roast_level: string | null
+          roaster_id: number | null
+          user_id: string | null
+        }
+        Insert: {
+          country?: string | null
+          cupping_score?: number | null
+          id?: string
+          is_public?: boolean | null
+          is_specialty?: boolean | null
+          name: string
+          notes?: string | null
+          price?: number | null
+          processing?: string | null
+          roast_date?: string | null
+          roast_level?: string | null
+          roaster_id?: number | null
+          user_id?: string | null
+        }
+        Update: {
+          country?: string | null
+          cupping_score?: number | null
+          id?: string
+          is_public?: boolean | null
+          is_specialty?: boolean | null
+          name?: string
+          notes?: string | null
+          price?: number | null
+          processing?: string | null
+          roast_date?: string | null
+          roast_level?: string | null
+          roaster_id?: number | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "coffees_roaster_id_fkey"
+            columns: ["roaster_id"]
+            isOneToOne: false
+            referencedRelation: "roasters"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      roasters: {
+        Row: {
+          countries: string[] | null
+          id: number
+          notes: string | null
+          title: string
+          url: string | null
+        }
+        Insert: {
+          countries?: string[] | null
+          id?: number
+          notes?: string | null
+          title: string
+          url?: string | null
+        }
+        Update: {
+          countries?: string[] | null
+          id?: number
+          notes?: string | null
+          title?: string
+          url?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {},
+  },
+} as const
